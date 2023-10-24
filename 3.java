64e83949afbad8bb61711e5633c65e1fa4da3cc3
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-10-25 00:14:07 +0530

    3.java

---
+++ 3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("The sum of three numbers is: ");
+        int a=10;
+        int b=5;
+        int c=6;
+        int sum=a+b+c;
+        System.out.print(sum);
+    }
+}
