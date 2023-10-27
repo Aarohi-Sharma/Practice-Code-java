b6833f5bc6a422d3f5b6f8f900f409ae2b42b5b4
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-10-28 00:18:54 +0530

    Main_04.java

---
+++ Main_04.java
@@ -0,0 +1,13 @@
+public class Main_04 {
+    public static void main(String args[]) {
+        int a=34;
+        byte b=89;
+        short c=7899;
+        float d=9.78f;
+        long e=86723573949L;
+        char f='I';
+        double g= 6734.9374940;
+        String str="age";
+        System.out.print(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+str);
+    }
+}
