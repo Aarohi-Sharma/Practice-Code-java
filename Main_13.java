c217b7edf32e3589e0d82ff4669e4117e60cb656
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-10-31 00:09:20 +0530

    Main_13.java

--- Main_13.java
+++ Main_13.java
@@ -1,5 +1,10 @@
 public class Main_13 {
     public static void main(String args[]) {
-        
+        String name="riya";
+        System.out.println("The name is:" +name);
+        int a=6;
+        float b=5.4678f;
+        System.out.println("value of a="+a+", value of b="+b);
+        System.out.printf("value of a=%d, value of b=%f", a, b);
     }
 }
