04eacbb3d1b8a57424183c54c6b4ac46679629f1
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-10-30 23:37:03 +0530

    Main_10.java

---
+++ Main_10.java
@@ -0,0 +1,17 @@
+public class Main_10 {
+    public static void main(String args[]) {
+        int i=56;
+        int j=67;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(j++*2);
+        System.out.println(j);
+        System.out.println(++j*2);
+        System.out.println(j);
+        char ch='a';
+        System.out.println(++ch);
+        System.out.println(ch++);
+    }
+}
