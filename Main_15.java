a9d7abda4274ebc6d13692010f2bf173889bfb63
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-01 15:45:10 +0530

    Main_15.java

---
+++ Main_15.java
@@ -0,0 +1,12 @@
+public class Main_15 {
+    public static void main( String args[]) {
+        String str= "I Live in DDN";
+        System.out.println(str.toLowerCase());
+        System.out.println(str.replace(' ', '_'));
+        System.out.println(str.replace("DDN", "Noida"));
+        String str2="this is a   string";
+        System.out.println(str2.indexOf("  "));
+        System.out.println(str2.indexOf("   "));
+        System.out.println("Hello all, \n\tThis is fun.\nThanks");
+    }
+}
