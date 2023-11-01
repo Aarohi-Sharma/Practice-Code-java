573358e760c2f6ee4f223fbcc0347e5dff0cd30f
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-01 15:55:52 +0530

    Main_16.java

---
+++ Main_16.java
@@ -0,0 +1,18 @@
+public class Main_16 {
+    public static void main(String args[]) {
+        int age=18;
+        if (age>=18){
+            System.out.println("legal");
+        }
+        else{
+            System.out.println("illegal");
+        }
+        boolean cond=(age==18);
+        if (cond){
+            System.out.println("adult");
+        }
+        else{
+            System.out.println("not adult");
+        }
+    }
+}
