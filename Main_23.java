f725a3ef2497254f9328e3a43deb80a94eaa63f7
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-02 16:45:33 +0530

    Main_23.java

---
+++ Main_23.java
@@ -0,0 +1,19 @@
+public class Main_23 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+        for (int a=0; a<10; a++) {
+            System.out.println(2*a+1);
+        }
+        for (int i=5; i>0; i--){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+        String name="rajveer";
+        System.out.println();
+        for (int j=0; j<name.length(); j++) {
+            System.out.println(name.charAt(j));
+        }
+    }
+}
