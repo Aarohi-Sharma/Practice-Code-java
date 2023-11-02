a43289e970ce7053b09d805b5cfce8fcfaf0ec61
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-02 16:23:43 +0530

    Main_21.java

---
+++ Main_21.java
@@ -0,0 +1,15 @@
+public class Main_21 {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=5){
+            System.out.println(i);
+            i++;
+        }
+        int j=100;
+        while (j<=200){
+            System.out.print(j);
+            System.out.print(" ");
+            j++;
+        }
+    }
+}
