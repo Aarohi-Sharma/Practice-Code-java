858585d562199af09046ac37fcc192f7cc9d319c
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-03 18:40:24 +0530

    Main_26.java

---
+++ Main_26.java
@@ -0,0 +1,14 @@
+public class Main_26 {
+    public static void main(String[] args) {
+        int[] marks=new int[5];
+        marks[0]=100;
+        marks[1]=64;
+        marks[2]=56;
+        marks[3]=73;
+        marks[4]=23;
+        System.out.println(marks[3]);
+        char[] name={'a', 'b', 'c', 'd', 'e'};
+        System.out.println(name[3]);
+        System.out.println(name.length);
+    }
+}
