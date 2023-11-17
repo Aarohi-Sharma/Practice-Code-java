1d84eb84c31ff13ef62f15a4467d15372ea0dda8
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-17 23:19:46 +0530

    Usage.java

---
+++ Self Practice/Usage.java
@@ -0,0 +1,8 @@
+package hello;
+import hello.Hello;
+public class Usage{
+    public static void main(String[] args){
+        System.out.println("Using packages:");
+        Hello n= new Hello();
+    }
+}
