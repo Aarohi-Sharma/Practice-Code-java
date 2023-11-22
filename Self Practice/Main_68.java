0ee5c6e31ff8687ca207998a555b86cd4a7cd934
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-22 22:10:09 +0530

    Main_68.java

---
+++ Self Practice/Main_68.java
@@ -0,0 +1,8 @@
+package com.shape;
+import com.shape.Rectangle;
+public class Main_68{
+    public static void main(String[] main){
+        Rectangle c=new Rectangle(7, 6);
+        System.out.println("Area of rectangle is "+c.area());
+    }
+}
