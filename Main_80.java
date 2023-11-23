f103e9bb8b9a30702fc8581b20bd4e02cffcbabc
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-11-23 17:32:49 +0530

    Main_80.java

---
+++ Main_80.java
@@ -0,0 +1,13 @@
+public class Main_80 {
+    public static void main(String[] args) {
+        int a=6;
+        int b=0;
+        int c;
+        try {
+            c = a / b;
+        }
+        catch(Exception e){
+            System.out.println("Error is "+e);
+        }
+    }
+}
