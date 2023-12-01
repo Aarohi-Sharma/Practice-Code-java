f84d8d7413d93bfecd6e241366e102133ad29d96
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-12-01 22:38:19 +0530

    Main_95.java

---
+++ Main_95.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+public class Main_95 {
+    public static void main(String[] args) {
+        HashSet<Integer> h1= new HashSet<>(6, 0.5f);
+        h1.add(9);
+        h1.add(87);
+        h1.add(11);
+        h1.add(23);
+        h1.add(54);
+        h1.add(34);
+        System.out.println(h1);
+    }
+}
