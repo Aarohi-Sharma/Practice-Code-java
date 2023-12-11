09107396370017804c69e888b820cc86ea844e24
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-12-11 23:48:52 +0530

    Str1.java

---
+++ End Sems/String/Str1.java
@@ -0,0 +1,14 @@
+public class Str1 {
+    public static void main(String[] args) {
+        String age="9";
+        String s="he is "+age;
+        System.out.println(s);
+        int ag=9;
+        String s1="he is "+ag;
+        System.out.println(s1);
+        String s3 = "football";
+        System.out.println(s3.endsWith("all"));
+        System.out.println(s3.startsWith("foo"));
+    }
+
+}
