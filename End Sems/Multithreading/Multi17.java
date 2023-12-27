052e5b4294d34377d650e24dc49202a4e0c70222
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2023-12-27 22:02:44 +0530

    Multi17.java

---
+++ End Sems/Multithreading/Multi17.java
@@ -0,0 +1,40 @@
+class Table1
+{
+    void printTable(int n){
+        synchronized(this){//synchronized block
+            for(int i=1;i<=5;i++){
+                System.out.println(n*i);
+                try{
+                    Thread.sleep(400);
+                }catch(Exception e){System.out.println(e);}
+            }
+        }
+    }
+}
+class MyThread1 extends Thread{
+    Table2 t;
+    MyThread1(Table2 t){
+        this.t=t;
+    }
+    public void run(){
+        t.printTable(5);
+    }
+}
+class MyThread2 extends Thread{
+    Table2 t;
+    MyThread2(Table2 t){
+        this.t=t;
+    }
+    public void run(){
+        t.printTable(100);
+    }
+}
+public class Multi17{
+    public static void main(String args[]){
+        Table2 obj = new Table2();//only one object
+        MyThread1 t1=new MyThread1(obj);
+        MyThread2 t2=new MyThread2(obj);
+        t1.start();
+        t2.start();
+    }
+}
