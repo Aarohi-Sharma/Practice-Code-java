1764543a12062f5bbe938f23f853f388153f2369
Aarohi Sharma <136839094+Aarohi-Sharma@users.noreply.github.com>
2024-01-05 23:55:11 +0530

    Wc1.java

---
+++ End Sems/Wrapper Classes/Wc1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+public class Wc1 {
+    public static void main(String args[]) {
+        int a = 20;
+        Integer i = Integer.valueOf(a);
+        Integer j = a;
+        System.out.println(a + " " + i + " " + j);
+    }
+}
